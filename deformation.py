1c9e6e560472068ed496ce6af4f250aae1c01718
HMS-ELKHOLY <hussien.m.elkholy@gmail.com>
2020-01-25 10:53:56 +0200

    comment

--- deformation.py
+++ deformation.py
@@ -1,23 +1,36 @@
 import numpy as np
 import time
-from scipy import integrate
-x = np.linspace(0, np.pi*2.0,4097)
-y = np.square(np.square(np.sin(x)))
-print(x)
-integrate.romb(y,dx=np.pi*2.0/4097)
+ from scipy import integrate
+# ##############integration tutriall
+# x = np.linspace(0, np.pi*2.0,4097)
+# y = np.square(np.square(np.sin(x)))
+# print(x)
+
+# integrate.romb(y,dx=np.pi*2.0/4097)
+
+#best value for samples romb integration is >  4096
+#note about romb : samples number must be in form 2**k+1
 def calc_speed(arrx,step,arry=None,arrz=None):
-    if(arry!=None and arrz!=None ):
+    if(type(arry)!=None and type(arrz)!=None ):
           return (integrate.romb(arrx,dx=step),   integrate.romb(arry,dx=step) ,   integrate.romb(arrz,dx=step))
     return integrate.romb(arrx,dx=step)
 def add_acc_val(arrx,combred_time,period,arry=None,arrz=None):
+    """
+    get values from  sensor
+    
+    """
+    global counter
+    
     while time.time()-combred_time<period :
         pass 
-    arrx.append(1)##use sensor library instead
-    if(arry!=type(None)):
-        arry.append(1)##use sensor library instead
-    if(arry!=type(None)):
-        arrz.append(1)##use sensor library instead
-
+    arrx[0,counter]=1##use sensor library instead
+    if(type(arry)!=type(None)):
+        arry[0,counter]=1##use sensor library instead
+    if(type(arrz)!=type(None)):
+        arrz[0,counter]=1##use sensor library instead
+    counter=counter+1
+    if counter>4096:
+        counter=0
 def smooth_accl(arrx,arry=None,arrz=None):
     maxX=np.max(arrx)
     print(maxX)
@@ -32,8 +45,13 @@ def smooth_accl(arrx,arry=None,arrz=None):
     if type(arrz) !=type(None) :
         for x in range(len(arrz)) :
             maxZ=np.max(arrz)
-            arry[x]=arry[x]*np.abs(arry[x])/maxY
+            arry[x]=arry[x]*np.abs(arry[x])/maxZ
 def detect_humbs(arrz,arrx):
+    """
+    humps make change in z axis in short period
+    make sure that max vlaue of hump is about 0.8g (car is falling )
+    make sur that hump occured in only small amount of time
+    """
     if np.abs(np.max(arrz)) > 0.8*9.8 :
         low = np.where(arrz < 0.15*9.8)
         if len(low)/len(arrz) >= 0.7:
@@ -47,7 +65,7 @@ def detect_humbs(arrz,arrx):
     
     
     
-#########################test##################################    
+######################### 1st test  ##################################    
 arr=np.ones((1,20))
 
 arr[0,5]=1
@@ -63,24 +81,30 @@ detect_humbs(arr,arr2)
 ##################################################################
 
 
-###############what  happens in project########################
+###############  what  happens in project  ########################
+
+speedAcc=0
 #########read gps first time
 #######sleep 4 seconds by reading accelration
 start=time.time()
-arrx=list()
-arry=list()
-arrz=list()
+arrx=np.zeros((1,4097))
+arry=np.zeros((1,4097))
+arrz=np.zeros((1,4097))
 print("2nd test")
 for counter in range (4097):
     add_acc_val(arrx,time.time(),0.001,arry,arrz)
 end=time.time()#########extra seconds why?????????????????
 
 print(end-start)
-for i in range (10,100):
-    arrz[i]=1.5*9.8##########make humps
+
+arrz[0,10:50]=2*9.8##########make humps by software
+print(arrz.shape)
+
 smooth_accl(arrx,arry,arrz)
-detect_humbs(arrz,arrx)
+detect_humbs(arrz,arrx)#hump detect
 speed_accelometer=calc_speed(arrx,.001,arry,arrz)
+
 print(speed_accelometer)
+
 #########read gps second time
 ######apply kalman filter 
