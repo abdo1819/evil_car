86509f654a6c653bb85f2c0e95e6f9b72e300d8e
HUSSIEN-ELKHOLY <hussien.m.elkholy@gmail.com>
2020-03-02 08:36:11 +0200

    multi thread hump detect

--- accel.py
+++ accel.py
@@ -6,6 +6,7 @@ import time
 import board
 import busio
 import adafruit_adxl34x
+from Queue import Queue
 # ##############integration tutriall
 # x = np.linspace(0, np.pi*2.0,4097)
 # y = np.square(np.square(np.sin(x)))
@@ -29,7 +30,7 @@ class accel:
             return (integrate.romb(arrx,dx=step),   integrate.romb(arry,dx=step) ,   integrate.romb(arrz,dx=step))
         return integrate.romb(arrx,dx=step)
 
-    def add_acc_val(self,arrx,combred_time,period,arry=None,arrz=None):
+    def add_acc_val(self,arrx,combred_time,period,arry=None,arrz=None,long_arr=None,lat_arr=None):
         """
         get values from  sensor
         
@@ -42,6 +43,11 @@ class accel:
             arry[0,self.counter]=self.accelerometer.acceleration[1]
         if(type(arrz)!=type(None)):
             arrz[0,self.counter]=self.accelerometer.acceleration[2]
+        if(type(lat_arr)!=type(None)):
+            lat_arr[counter]=0#read gps _____________________review kerolos girgs code__________________
+        if(type(long_arr)!=type(None)):
+            long_arr[counter]=0#read gps _____________________review kerolos girgs code__________________
+            
         self.counter=self.counter+1
         if self.counter>4096:
             self.counter=0
@@ -58,7 +64,7 @@ class accel:
         if type(arrz) !=type(None) :
             maxZ=np.max(arrz)+1
             arrz=arrz*np.abs(arrz)/maxZ
-    def detect_humbs(self,arrz,arrx):
+    def detect_humbs(self,arrz,arrx,long_arr,lat_arr):
         """
         humps make change in z axis in short period
         make sure that max vlaue of hump is about 0.8g (car is falling )
@@ -68,14 +74,10 @@ class accel:
             low = np.where(arrz < 0.15*9.8)
             if len(low)/len(arrz) >= 0.7:
                 print ("humb")
-                return True
+                return True , (lat_arr(np.argmax(arrz)),long_arr(np.argmax(arrz)))
         print("noHumps")
-        return False    
-                
-        
-        
-        
-        
+        return False , (lat_arr(np.argmax(arrz)),long_arr(np.argmax(arrz))),np.argmax(arrz)
+    
     def test(self):        
         ######################### 1st test  ##################################    
         arr=np.ones((1,20))
@@ -91,40 +93,54 @@ class accel:
         print(arr2)        
         self.detect_humbs(arr,arr2)
         ##################################################################
-
-
     ###############  what  happens in project  ########################
-
-    
     def get_humb_speed(self):
-
         # speedAcc=0
-        #########read gps first time
+        #########read gps _____________________review kerolos girgs code__________________
+        ##first time
         #######sleep 4 seconds by reading accelration
         # start=time.time()
         while True:
-
             arrx=np.zeros((1,4097))
             arry=np.zeros((1,4097))
             arrz=np.zeros((1,4097))
+            arr_long=np.zeros((1,4097))
+            arr_lat=np.zeros((1,4097))
             print("2nd test")
             for i in range (4097):
-                self.add_acc_val(arrx,time.time(),0.001/6,arry,arrz)
-                # end=time.time()#########extra seconds why?????????????????
-
-
-
-            
-
-
+                self.add_acc_val(arrx,time.time(),0.001/12,arry,arrz,arr_long,arr_lat)
+                # end=time.time()#########extra seconds why?????????????????         
             self.smooth_accl(arrx,arry,arrz)
-            humbs = self.detect_humbs(arrz,arrx)#hump detect
-            speed_accelometer=self.calc_speed(arrx,.001/6,arry,arrz)
-            print('speed__',speed_accelometer)
-            speed=np.sqrt(np.power(speed_accelometer[1],2)+np.power(speed_accelometer[0],2))            
-            print("__total",speed)
-            return humbs,speed_accelometer[0:1]
+            return self.detect_humbs(arrz,arrx)#hump detect
         
         # print(speed_accelometer)
-    #########read gps second time
-    ######apply kalman filter 
+    #########read gps _____________________review kerolos girgs code__________________
+      ##  second time
+    ######apply kalman filter
+queue_accl=Queue(10)
+class accelThreadProduce :
+    def __init__(self):
+        self.humb=false
+        self.speed=(0,0)
+        self.reader=accel()
+        super(accelThread, self).__init__()
+        
+    def run(self):
+        
+        while True:
+            global queue_accl 
+            queue_accl.put(reader.get_humb_speed())
+            
+            
+        
+class accelThreadConsume(Thread):
+    def run(self):
+        global queue
+        while True:
+            values = queue_accl.get()
+            queue_accl.task_done()
+            if values[1]==True:
+                ##gsm invoke _____review salma emad code________
+                
+                
+
